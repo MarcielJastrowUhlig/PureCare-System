a1e4de73e75cc68f3239e7880a3d903729303514
Marciel Jastrow Uhlig <marcieluhlig@gmail.com>
2023-09-13 19:55:16 -0300

    Teste

---
+++ alimentacao.java
@@ -0,0 +1,9 @@
+
+public class alimentacao{
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
+
+
---
+++ usuario.java
@@ -0,0 +1,45 @@
+package pureCareSystem;
+
+// Atributos: ID, nome de usuário, senha, email.
+
+public class Usuario {
+	private String nomeUsuario;
+	private String senhaUsuario;
+	private String emailUsuario;
+	
+	public Usuario(String nomeUsuario, String senhaUsuario, String emailUsuario) {
+		this.nomeUsuario = nomeUsuario;
+		this.senhaUsuario = senhaUsuario;
+		this.emailUsuario = emailUsuario;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [nomeUsuario=" + nomeUsuario + ", senhaUsuario=" + senhaUsuario + ", emailUsuario="
+				+ emailUsuario + "]";
+	}
+
+	public String getNomeUsuario() {
+		return nomeUsuario;
+	}
+
+	public void setNomeUsuario(String nomeUsuario) {
+		this.nomeUsuario = nomeUsuario;
+	}
+
+	public String getSenhaUsuario() {
+		return senhaUsuario;
+	}
+
+	public void setSenhaUsuario(String senhaUsuario) {
+		this.senhaUsuario = senhaUsuario;
+	}
+
+	public String getEmailUsuario() {
+		return emailUsuario;
+	}
+
+	public void setEmailUsuario(String emailUsuario) {
+		this.emailUsuario = emailUsuario;
+	}
+}
